b5d58afbde21ebd80dd7d64dba30483ea9c9a89c
huangbin <huangbin@snqu.com>
2022-11-20 17:36:04 +0800

    主题设置

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import { useRoutes } from 'react-router-dom';
 import rootRouter from './routers';
 import '@/App.scss'
+import { useEffect } from 'react';
+import useTheme from './hooks/theme';
 
 function App() {
   const AppRoutes = () => useRoutes(rootRouter);
+
+  /** 主题修改 */
+  const { theme } = useTheme();
+  useEffect(() => {
+    document.documentElement.setAttribute(
+      "data-prefers-color-scheme",
+      theme ?? "light"
+    );
+  }, [theme]);
 
   return (<><AppRoutes /></>)
 }
